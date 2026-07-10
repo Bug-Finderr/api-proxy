e9b83e29fe8426ebbbb8a05743fe2a9df21cdab0
Sudharsan <sudu.boi.2k5@gmail.com>
2026-07-10 15:54:20 +0530

    chore: tighten project checks

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["test/**/*.test.ts"],
-    exclude: ["test/sdk-compat/**", "node_modules/**"],
+    exclude: ["test/sdk-compat/**"],
   },
   plugins: [
     cloudflareTest({
